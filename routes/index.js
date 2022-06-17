29b80d889f5793dab48908f85a86d073e4e63fe9
Chris Bowen <50932159+mmxchris@users.noreply.github.com>
2022-06-17 10:09:47 -0400

    Revert "commit"
    This reverts commit f8649f5480a07b58aab487fd4db84835f9c9c3bd.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const { route } = require('./meatSurvey');
 const routes = require('express').Router();
-const {ensureAuth} = require('../middleware/auth');
 
-routes.use('/meatDepartment',ensureAuth, require('./meatSurvey'));
-routes.use('/produceDepartment', ensureAuth, require('./produceSurvey'));
+routes.use('/meatDepartment', require('./meatSurvey'));
+routes.use('/produceDepartment', require('./produceSurvey'));
 routes.get('/', (req, res) => {
     res.send('Home');
   });
