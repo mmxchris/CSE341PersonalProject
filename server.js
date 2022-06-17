a0cc1ad5bea6c310d2bea526d4b8b01caabf42f7
Chris Bowen <50932159+mmxchris@users.noreply.github.com>
2022-06-17 10:10:19 -0400

    Revert "OAuth added"
    This reverts commit 9bb26304928e6c6519c442c7c77a72da5f5560cb.

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-const passport = require('passport');
-const session = require('express-session')
 const connectionDB = require('./DB/connection');
 const bodyParser = require('body-parser');
 
@@ -11,22 +9,9 @@ connectionDB();
 app.use(bodyParser.json());
 app.use('/', require('./routes'));
 
-//session
-app.use(session({
-    secret: 'this is a secret',
-    resave: false,
-    saveUninitialized: false
-}))
-
-//passport
-require('./helper/passport')(passport);
-app.use(passport.initialize());
-app.use(passport.session());
-
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use('/auth', require('./routes/auth'));
 
 app.listen(port, () => [
      console.log(`Surver running on port ${port}`)
